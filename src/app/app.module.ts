86bebc6fc8b9100964b5427c449796ed12182939
bjohnso <bjohnso@student.wethinkcode.co.za>
2020-08-22 17:24:08 +0200

    Fix Routing, Module Config and Add localize for Angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { ExploreComponent } from './explore/explore.component';
 import { ChatComponent } from './chat/chat.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SharedComponent } from './shared/shared.component';
 import {CoreModule} from './core/core.module';
 import {AuthModule} from './auth/auth.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
-    LoginComponent,
-    RegisterComponent,
     ExploreComponent,
     ChatComponent,
     ProfileComponent,
     SharedComponent
   ],
   imports: [
+    NgbModule,
+    FormsModule,
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule,
     // CUSTOM
     CoreModule,
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RegisterComponent} from './register/register.component';
 import {LoginComponent} from './login/login.component';
+import {FormsModule} from '@angular/forms';
+import {AuthComponent} from './auth.component';
 
 @NgModule({
   declarations: [
+    AuthComponent,
     LoginComponent,
     RegisterComponent
   ],
-  exports: [LoginComponent, RegisterComponent]
+  imports: [
+    FormsModule
+  ],
+  exports: [AuthComponent, LoginComponent, RegisterComponent]
 })
 export class AuthModule {}
