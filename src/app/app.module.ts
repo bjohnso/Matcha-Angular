4f5c35087bd13efedff8ac4003181d92199c722f
bjohnso <bjohnso@student.wethinkcode.co.za>
2020-08-22 16:47:32 +0200

    Feature Scafolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CoreComponent } from './core/core.component';
+import { ExploreComponent } from './explore/explore.component';
+import { ChatComponent } from './chat/chat.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    CoreComponent,
+    ExploreComponent,
+    ChatComponent,
+    ProfileComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
