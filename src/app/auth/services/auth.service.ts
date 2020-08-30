cbbac1301a5a49aafea6ae1f606e5c2cd90562f3
justicemuch <justinjamesqwerty@gmail.com>
2020-08-30 15:30:50 +0200

    added auth to endpoint

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -9,12 +9,10 @@ export class AuthService {
 
   register(data: any) {
     // tslint:disable-next-line:new-parens
-    const headers: HttpHeaders = new HttpHeaders();
-    headers.append('Access-Control-Allow-Origin', '*');
     if (data.username && data.password && data.firstname && data.lastname && data.email) {
       return this.httpClient.post(
-        '/api/register/',
-        data, {headers});
+        '/api/auth/register/',
+        data);
     }
     return null;
   }
