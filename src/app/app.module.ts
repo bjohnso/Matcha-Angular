acb8a0b50ccddca24293229a91bd9452b58cfbbc
bjohnso <bjohnso@student.wethinkcode.co.za>
2020-09-01 18:03:21 +0200

    Added Shared Module with Header Component to house User settings controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExploreComponent } from './explore/explore.component';
 import { ChatComponent } from './chat/chat.component';
-import { ProfileComponent } from './profile/profile.component';
-import {JWTTokenService} from './services/jwt-token.service';
-import { SharedComponent } from './shared/shared.component';
 import {CoreModule} from './core/core.module';
 import {AuthModule} from './auth/auth.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { EditProfileComponent } from './profile/edit-profile/edit-profile.component';
 import {ProfileModule} from './profile/profile.module';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import {NgSelectModule} from '@ng-select/ng-select'
-import {environment} from '../environments/environment'
- 
-//temporary for testing
+import {NgSelectModule} from '@ng-select/ng-select';
+import {environment} from '../environments/environment';
+import { HeaderComponent } from './shared/header/header.component';
+import {SharedModule} from './shared/shared.module';
+
+// temporary for testing
+// tslint:disable-next-line:max-line-length
 const config: SocketIoConfig = { url: environment.api.baseURL, options: {query : {token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjIzLCJpYXQiOjE1OTg1MjI1MzR9.tYa0OB9dm0gLwABXofuMRIW45e8G06GbW1IH0kBeCes'}}};
 // const config: SocketIoConfig = { url: environment.api.baseURL, options: {query : {token : JWTTokenService.jwtToken}}};
 @NgModule({
@@ -27,7 +26,7 @@ const config: SocketIoConfig = { url: environment.api.baseURL, options: {query :
     AppComponent,
     ExploreComponent,
     ChatComponent,
-    SharedComponent,
+    HeaderComponent,
   ],
   imports: [
     NgbModule,
@@ -40,8 +39,9 @@ const config: SocketIoConfig = { url: environment.api.baseURL, options: {query :
     SocketIoModule.forRoot(config),
     // CUSTOM
     CoreModule,
+    SharedModule,
     AuthModule,
-    ProfileModule
+    ProfileModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
