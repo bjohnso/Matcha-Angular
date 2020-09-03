c6a1122d50238007f6959041d69d27d04f075be0
bjohnso <bjohnso@student.wethinkcode.co.za>
2020-09-03 22:13:22 +0200

    Edit Profile - Field Mappings, Input/Link/Event Handling, ViewModel Updating

---
+++ src/app/profile/constants/input-fields.constant.ts
@@ -0,0 +1,58 @@
+export const EDIT_PROFILE_FIELDS = {
+  FIRST_NAME: {
+    INPUT_ID: 'inputFirstname',
+    LINK_ID: 'linkFirstname',
+    ID_SUFFIX: 'Firstname',
+    PROP: 'firstname',
+    VALIDATION_ID: 'validationFirstname'
+  },
+  LAST_NAME: {
+    INPUT_ID: 'inputLastname',
+    LINK_ID: 'linkLastname',
+    ID_SUFFIX: 'Lastname',
+    PROP: 'lastname',
+    VALIDATION_ID: 'validationLastname'
+  },
+  USERNAME: {
+    INPUT_ID: 'inputUsername',
+    LINK_ID: 'linkUsername',
+    ID_SUFFIX: 'Username',
+    PROP: 'username',
+    VALIDATION_ID: 'validationUsername'
+  },
+  EMAIL: {
+    INPUT_ID: 'inputEmail',
+    LINK_ID: 'linkUEmail',
+    ID_SUFFIX: 'Email',
+    PROP: 'email',
+    VALIDATION_ID: 'validationEmail'
+  },
+  GENDER: {
+    INPUT_ID: 'inputGender',
+    LINK_ID: 'linkGender',
+    ID_SUFFIX: 'Gender',
+    PROP: 'gender',
+    VALIDATION_ID: 'validationGender'
+  },
+  SEXUAL_PREFERENCE: {
+    INPUT_ID: 'inputSexualPreference',
+    LINK_ID: 'linkSexualPreference',
+    ID_SUFFIX: 'SexualPreference',
+    PROP: 'sexualPreference',
+    VALIDATION_ID: 'validationSexualPreference'
+  },
+  SEXUAL_ORIENTATION: {
+    INPUT_ID: 'inputSexualOrientation',
+    LINK_ID: 'linkSexualOrientation',
+    ID_SUFFIX: 'SexualOrientation',
+    PROP: 'sexualOrientation',
+    VALIDATION_ID: 'validationSexualOrientation'
+  },
+  BIOGRAPHY: {
+    INPUT_ID: 'inputDescription',
+    LINK_ID: 'linkDescription',
+    ID_SUFFIX: 'Description',
+    PROP: 'description',
+    VALIDATION_ID: 'validationDescription'
+  }
+};
--- src/app/profile/edit-profile/edit-profile.component.ts
+++ src/app/profile/edit-profile/edit-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CoreComponent} from '../../core/core.component';
 import {SEXUAL_PREFERENCE, GENDER, SEXUAL_ORIENTATION} from '../../data/profile.data';
+import {EDIT_PROFILE_FIELDS} from '../constants/input-fields.constant';
 import {Profile} from '../models/profile.model';
 
 @Component({
@@ -10,15 +11,36 @@ import {Profile} from '../models/profile.model';
 })
 export class EditProfileComponent extends CoreComponent implements OnInit {
 
-  protected GENDER = GENDER;
-  protected SEXUAL_PREFERENCE = SEXUAL_PREFERENCE;
-  protected SEXUAL_ORIENTATION = SEXUAL_ORIENTATION;
-  protected profile: Profile;
+  // META-DATA
+  GENDER_OPTIONS = GENDER;
+  SEXUAL_PREFERENCE_OPTIONS = SEXUAL_PREFERENCE;
+  SEXUAL_ORIENTATION_OPTIONS = SEXUAL_ORIENTATION;
+  EDIT_PROFILE_FIELDS = EDIT_PROFILE_FIELDS;
+
+  // MODELS
+  profile: Profile = new Profile();
   constructor() {
     super();
   }
 
-  inputEvent() {
+  onInputEvent(event: Event, item?: any) {
+    if (event.type === 'input') {
+      this.textInputUpdate(event);
+    } else if (event.type === 'click') {
+      console.log('click');
+      const elem: HTMLElement = (event.target as HTMLElement);
+      const id: string = (elem.id as string);
+      const fieldsProperty = Object.keys(EDIT_PROFILE_FIELDS)
+        .find(prop => id.includes(EDIT_PROFILE_FIELDS[prop].LINK_ID));
+      if (fieldsProperty && item) {
+        const profileProperty = EDIT_PROFILE_FIELDS[fieldsProperty].PROP;
+        this.profile[profileProperty] = item;
+      }
+    }
+  }
+
+  textInputUpdate(event) {
+
   }
 
   ngOnInit(): void {
