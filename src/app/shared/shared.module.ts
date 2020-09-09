f8de367128bb9e987e8b04313931c367d19ef3cc
bjohnso <bjohnso@student.wethinkcode.co.za>
2020-09-09 17:56:01 +0200

    Chat Nesting in header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,16 @@ import {CommonModule} from '@angular/common';
 import { ProfileHeaderComponent } from './profile-header/profile-header.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, ProfileHeaderComponent],
-  imports: [
-    CommonModule,
-    FontAwesomeModule,
-    NgbDropdownModule
-  ],
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+        NgbDropdownModule,
+        RouterModule
+    ],
   exports: [HeaderComponent, ProfileHeaderComponent]
 })
 export class SharedModule {}
